d1dc4730afdea30a86ccdc6ee605000eade43e74
AnesBendimerad <bendimeradanes@gmail.com>
2015-12-06 15:37:18 +0100

    [Anes] : add stdafx

--- PDC3_BloomFilter/DataBaseHandler.cpp
+++ PDC3_BloomFilter/DataBaseHandler.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include "DataBaseHandler.h"
 
 DataBaseHandler::DataBaseHandler(DataBaseConfiguration dataBaseConfiguration)
--- PDC3_BloomFilter/DocumentIterator.cpp
+++ PDC3_BloomFilter/DocumentIterator.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include "DocumentIterator.h"
 
 DocumentIterator::DocumentIterator(CassResult * result)
