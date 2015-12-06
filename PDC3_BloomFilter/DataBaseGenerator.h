f064475ad47a49f95a5669ec0075907a6092c98d
Hassan El-Moutaraji <hassan.elmoutaraji@gmail.com>
2015-12-06 21:58:41 +0100

    [Hassan] DataBaseGenerator impelementation

--- PDC3_BloomFilter/DataBaseGenerator.h
+++ PDC3_BloomFilter/DataBaseGenerator.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <cassandra.h>
 #include "DataBaseConfiguration.h"
+
+unsigned const int documentNumberSize = 10;
+unsigned const int documentTypeSize = 10;
+unsigned const int countryCodeSize = 3;
+
 class DataBaseGenerator {
 private:
 	DataBaseConfiguration dataBaseConfiguration;
+	unsigned int dataBaseSize;
+	string RandomString(int len);
+	bool executeQuery(string query);
+
 public:
 	DataBaseGenerator(DataBaseConfiguration dataBaseConfiguration, unsigned int dataBaseSize);
 	bool generate();
