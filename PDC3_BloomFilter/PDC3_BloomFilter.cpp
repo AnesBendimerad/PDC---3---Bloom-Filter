8a9f3b963afa3c5eb467259a39fab0d09af30347
AnesBendimerad <bendimeradanes@gmail.com>
2015-12-06 14:05:18 +0100

    commit first

---
+++ PDC3_BloomFilter/PDC3_BloomFilter.cpp
@@ -0,0 +1,70 @@
+// PDC3_BloomFilter.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cassandra.h>
+#include <conio.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	/* Setup and connect to cluster */
+	CassCluster* cluster = cass_cluster_new();
+	CassSession* session = cass_session_new();
+
+	/* Add contact points */
+	cass_cluster_set_contact_points(cluster, "127.0.0.1");
+
+	/* Provide the cluster object as configuration to connect the session */
+	CassFuture* connect_future = cass_session_connect(session, cluster);
+
+	/* This operation will block until the result is ready */
+	CassError rc = cass_future_error_code(connect_future);
+
+	printf("Connect result: %s\n", cass_error_desc(rc));
+
+	/* Run queries... */
+
+	/* Build statement and execute query */
+	CassStatement* statement
+		= cass_statement_new("SELECT *"
+			"FROM hr.emp", 0);
+
+	CassFuture* result_future = cass_session_execute(session, statement);
+
+	if (cass_future_error_code(result_future) == CASS_OK) {
+		/* Retrieve result set and iterate over the rows */
+		const CassResult* result = cass_future_get_result(result_future);
+		CassIterator* rows = cass_iterator_from_result(result);
+
+		while (cass_iterator_next(rows)) {
+			const CassRow* row = cass_iterator_get_row(rows);
+			const CassValue* value = cass_row_get_column_by_name(row, "emp_first");
+
+			const char* keyspace;
+			size_t keyspace_length;
+			cass_value_get_string(value, &keyspace, &keyspace_length);
+			printf("Name: '%.*s'\n", (int)keyspace_length, keyspace);
+		}
+
+		cass_result_free(result);
+		cass_iterator_free(rows);
+	}
+	else {
+		/* Handle error */
+		const char* message;
+		size_t message_length;
+		cass_future_error_message(result_future, &message, &message_length);
+		fprintf(stderr, "Unable to run query: '%.*s'\n",
+			(int)message_length, message);
+	}
+
+	cass_statement_free(statement);
+	cass_future_free(connect_future);
+	cass_session_free(session);
+	cass_cluster_free(cluster);
+	system("pause");
+	return 0;
+}
---
+++ PDC3_BloomFilter/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : PDC3_BloomFilter Project Overview
+========================================================================
+
+AppWizard has created this PDC3_BloomFilter application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your PDC3_BloomFilter application.
+
+
+PDC3_BloomFilter.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+PDC3_BloomFilter.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+PDC3_BloomFilter.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named PDC3_BloomFilter.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
