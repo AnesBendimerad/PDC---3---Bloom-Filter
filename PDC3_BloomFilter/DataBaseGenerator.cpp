042668a190201481e2b8fdd1af5b454fd5ed4c32
BelfodilAimene <ba_belfodil@esi.dz>
2015-12-07 01:05:07 +0100

    [Aimene] DataBaseGenerator drop if exists

--- PDC3_BloomFilter/DataBaseGenerator.cpp
+++ PDC3_BloomFilter/DataBaseGenerator.cpp
@@ -14,13 +14,19 @@ DataBaseGenerator::DataBaseGenerator(DataBaseConfiguration dataBaseConfiguration
 
 bool DataBaseGenerator::generate()
 {
-	string keyspace = "create keyspace " + DataBaseGenerator::dataBaseConfiguration.keySpace + " with replication={'class':'SimpleStrategy', 'replication_factor':1};";
-	string index = " CREATE INDEX countryCode_index ON " + DataBaseGenerator::dataBaseConfiguration.keySpace + "." + DataBaseGenerator::dataBaseConfiguration.table + "(" + string(COUNTRY_CODE) + "); ";
-	string table = "create table " + DataBaseGenerator::dataBaseConfiguration.keySpace + "." + DataBaseGenerator::dataBaseConfiguration.table + " ( " + string(DOCUMENT_NUMBER) + " varchar primary key, " + string(DOCUMENT_TYPE) + " varchar, " + string(COUNTRY_CODE) + " varchar); ";
+	string dropKeyspace="drop keyspace if exists " + DataBaseGenerator::dataBaseConfiguration.keySpace + ";";
+	string createKeyspace = "create keyspace " + DataBaseGenerator::dataBaseConfiguration.keySpace + " with replication={'class':'SimpleStrategy', 'replication_factor':1};";
+	
+	string createTable = "create table " + DataBaseGenerator::dataBaseConfiguration.keySpace + "." + DataBaseGenerator::dataBaseConfiguration.table + " ( " + string(DOCUMENT_NUMBER) + " varchar primary key, " + string(DOCUMENT_TYPE) + " varchar, " + string(COUNTRY_CODE) + " varchar); ";
+	
+	string dropIndex = "drop index if exists " + string(COUNTRY_CODE) + "_index;";
+	string creatIndex = " create index " + string(COUNTRY_CODE) + "_index on " + DataBaseGenerator::dataBaseConfiguration.keySpace + "." + DataBaseGenerator::dataBaseConfiguration.table + "(" + string(COUNTRY_CODE) + "); ";
 
-	executeQuery(keyspace);
-	executeQuery(table);
-	executeQuery(index);
+	executeQuery(dropKeyspace);
+	executeQuery(createKeyspace);
+	executeQuery(createTable);
+	executeQuery(dropIndex);
+	executeQuery(creatIndex);
 
 	DataBaseHandler * dbh = new DataBaseHandler(DataBaseGenerator::dataBaseConfiguration);
 	Document doc;
