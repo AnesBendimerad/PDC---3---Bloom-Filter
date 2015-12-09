f99f97a5fd74cbf19956d81ffb5118c14f2f3757
AnesBendimerad <bendimeradanes@gmail.com>
2015-12-09 09:05:34 +0100

    [Anes] update of DataBaseGenerator

--- PDC3_BloomFilter/DataBaseGenerator.cpp
+++ PDC3_BloomFilter/DataBaseGenerator.cpp
@@ -1,13 +1,16 @@
 #include "stdafx.h"
 #include <time.h>
+#include <algorithm>
 #include "DataBaseGenerator.h"
 #include "Document.h"
 #include "DataBaseHandler.h"
+
 
 using namespace std;
 
 DataBaseGenerator::DataBaseGenerator(DataBaseConfiguration dataBaseConfiguration, unsigned int dataBaseSize)
 {
+	countryCode = loadCountryCode();
 	DataBaseGenerator::dataBaseConfiguration = dataBaseConfiguration;
 	DataBaseGenerator::dataBaseSize = dataBaseSize;
 }
@@ -18,10 +21,12 @@ bool DataBaseGenerator::createAndGenerateDB()
 	DataBaseHandler * dataBaseHandler = new DataBaseHandler(this->dataBaseConfiguration);
 	Document doc;
 	unsigned int i = 0;
+	srand(time(NULL));
+
 	while (i < dataBaseSize){
 		 
-		doc.documentNumber = std::to_string(i);
-		doc.countryCode = RandomString(countryCodeSize);
+		doc.documentNumber = RandomString(documentNumberSize);
+		doc.countryCode = RandomCountryCode();
 		doc.documentType = RandomString(documentTypeSize);
 		dataBaseHandler->addDocument(&doc);
 		i++;
@@ -32,7 +37,7 @@ bool DataBaseGenerator::createAndGenerateDB()
 
 string DataBaseGenerator::RandomString(unsigned int len)
 {
-	srand(time(0));
+	
 	string str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	unsigned int pos;
 	while (str.size() != len) {
@@ -40,6 +45,76 @@ string DataBaseGenerator::RandomString(unsigned int len)
 		str.erase(pos, 1);
 	}
 	return str;
+}
+
+std::vector<std::string> * DataBaseGenerator::loadCountryCode(){
+
+	std::vector<std::string> * countryCode = new std::vector<std::string>();
+
+	countryCode->push_back("ABW");
+	countryCode->push_back("AFG");
+	countryCode->push_back("AGO");
+	countryCode->push_back("AIA");
+	countryCode->push_back("ALA");
+	countryCode->push_back("ALB");
+	countryCode->push_back("AND");
+	countryCode->push_back("ARE");
+	countryCode->push_back("ARG");
+	countryCode->push_back("ARM");
+	countryCode->push_back("ASM");
+	countryCode->push_back("ATA");
+	countryCode->push_back("ATF");
+	countryCode->push_back("ATG");
+	countryCode->push_back("AUS");
+	countryCode->push_back("AUT");
+	countryCode->push_back("AZE");
+	countryCode->push_back("BDI");
+	countryCode->push_back("BEL");
+	countryCode->push_back("BEN");
+	countryCode->push_back("BES");
+	countryCode->push_back("BFA");
+	countryCode->push_back("BGD");
+	countryCode->push_back("BGR");
+	countryCode->push_back("BHR");
+	countryCode->push_back("BHS");
+	countryCode->push_back("BIH");
+	countryCode->push_back("BLM");
+	countryCode->push_back("BLR");
+	countryCode->push_back("BLZ");
+	countryCode->push_back("BMU");
+	countryCode->push_back("BOL");
+	countryCode->push_back("BRA");
+	countryCode->push_back("BRB");
+	countryCode->push_back("BRN");
+	countryCode->push_back("BTN");
+	countryCode->push_back("CMR");
+	countryCode->push_back("COD");
+	countryCode->push_back("COG");
+	countryCode->push_back("COK");
+	countryCode->push_back("COL");
+	countryCode->push_back("COM");
+	countryCode->push_back("CPV");
+	countryCode->push_back("CRI");
+	countryCode->push_back("CUB");
+	countryCode->push_back("CUW");
+	countryCode->push_back("CXR");
+	countryCode->push_back("CYM");
+	countryCode->push_back("CYP");
+	countryCode->push_back("CZE");
+	countryCode->push_back("DEU");
+	countryCode->push_back("DJI");
+	countryCode->push_back("MAR");
+	countryCode->push_back("MCO");
+	countryCode->push_back("MDA");
+	
+
+	return countryCode;
+}
+
+string DataBaseGenerator::RandomCountryCode(){
+
+	std::random_shuffle(countryCode->begin(), countryCode->end());
+	return countryCode->at((rand() % (countryCode->size() - 1)));
 }
 
 bool DataBaseGenerator::createDB(){
@@ -113,4 +188,5 @@ bool DataBaseGenerator::createDB(){
 
 DataBaseGenerator::~DataBaseGenerator()
 {
+	delete countryCode;
 }
--- PDC3_BloomFilter/DataBaseGenerator.h
+++ PDC3_BloomFilter/DataBaseGenerator.h
@@ -1,17 +1,21 @@
 #pragma once
 #include <cassandra.h>
 #include "DataBaseConfiguration.h"
+#include<vector>
 
-unsigned const int documentNumberSize = 10;
-unsigned const int documentTypeSize = 10;
+unsigned const int documentNumberSize = 25;
+unsigned const int documentTypeSize = 3;
 unsigned const int countryCodeSize = 3;
 
 class DataBaseGenerator {
 private:
 	DataBaseConfiguration dataBaseConfiguration;
 	unsigned int dataBaseSize;
+	std::vector<std::string> * countryCode;
 	string RandomString(unsigned int len);
 	bool createDB();
+	std::vector<std::string> * loadCountryCode();
+	string RandomCountryCode();
 
 public:
 	DataBaseGenerator(DataBaseConfiguration dataBaseConfiguration, unsigned int dataBaseSize);
